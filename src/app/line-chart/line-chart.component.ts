5dfc125757dd61ddc383be236945d39bbb75a9c3
Hooman Noroozinia <hooman.noroozinia@gallagher.com>
2022-06-08 11:46:55 +1000

    Fixing positioning

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -9,40 +9,43 @@ import * as d3 from "d3";
 export class LineChartComponent implements OnChanges {
   @Input() public data: { value: number; date: string }[] = [];
 
-  private width: number = 700;
-  private height: number = 700;
-  private margin: number = 50;
+  private width: number = 700; //inner width of the chart
+  private height: number = 400; //inner height of the chart
+  private margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+  };
   public svg: any;
   public svgInner: any;
   public yScale: any;
   public xScale: any;
-  public xAxis: any;
-  public yAxis: any;
+  public xAxisGroup: any;
+  public yAxisGroup: any;
   public lineGroup: any;
 
   constructor(public chartElem: ElementRef) {}
 
   initializeChart() {
-    this.svg = d3.select(this.chartElem.nativeElement).select(".linechart").append("svg").attr("height", this.height);
+    this.svg = d3
+      .select(this.chartElem.nativeElement)
+      .select(".linechart")
+      .append("svg")
+      .attr("height", this.height + this.margin.top + this.margin.bottom);
 
-    this.svgInner = this.svg.append("g").style("transform", "translate(" + this.margin + "px, " + this.margin + "px)");
-
-    this.yScale = d3
-      .scaleLinear()
-      .domain([<number>d3.max(this.data, d => d.value) + 1, <number>d3.min(this.data, d => d.value) - 1])
-      .range([0, this.height - 2 * this.margin]);
+    this.svgInner = this.svg.append("g").attr("transform", `translate(${this.margin.left},${this.margin.top})`);
 
     this.xScale = d3.scaleTime().domain(<[Date, Date]>d3.extent(this.data, d => new Date(d.date)));
 
-    this.yAxis = this.svgInner
-      .append("g")
-      .attr("id", "y-axis")
-      .style("transform", "translate(" + this.margin + "px, 0)");
+    this.yScale = d3
+      .scaleLinear()
+      .domain(<[number, number]>d3.extent(this.data, d => d.value))
+      .range([this.height, 0]);
 
-    this.xAxis = this.svgInner
-      .append("g")
-      .attr("id", "x-axis")
-      .style("transform", "translate(0, " + (this.height - 2 * this.margin) + "px)");
+    this.xAxisGroup = this.svgInner.append("g").attr("id", "x-axis").attr("transform", `translate(0,${this.height})`);
+
+    this.yAxisGroup = this.svgInner.append("g").attr("id", "y-axis");
 
     this.lineGroup = this.svgInner
       .append("g")
@@ -50,7 +53,25 @@ export class LineChartComponent implements OnChanges {
       .attr("id", "line")
       .style("fill", "none")
       .style("stroke", "red")
-      .style("stroke-width", "2px");
+      .style("stroke-width", 1);
+  }
+
+  drawChart() {
+    this.width = this.chartElem.nativeElement.getBoundingClientRect().width - this.margin.left - this.margin.right;
+    this.svg.attr("width", this.width + this.margin.left + this.margin.right);
+
+    this.xScale.range([0, this.width]);
+
+    this.xAxisGroup.call(d3.axisBottom(this.xScale));
+
+    this.yAxisGroup.call(d3.axisLeft(this.yScale));
+
+    const line = d3
+      .line()
+      .x(d => d[0])
+      .y(d => d[1]);
+    const points: [number, number][] = this.data.map(d => [this.xScale(new Date(d.date)), this.yScale(d.value)]);
+    this.lineGroup.attr("d", line(points));
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -59,26 +80,5 @@ export class LineChartComponent implements OnChanges {
       this.drawChart();
       window.addEventListener("resize", () => this.drawChart());
     }
-  }
-
-  drawChart() {
-    this.width = this.chartElem.nativeElement.getBoundingClientRect().width;
-    this.svg.attr("width", this.width);
-
-    this.xScale.range([this.margin, this.width - 2 * this.margin]);
-
-    const xAxis = d3.axisBottom(this.xScale).ticks(10);
-    this.xAxis.call(xAxis);
-
-    const yAxis = d3.axisRight(this.yScale);
-    this.yAxis.call(yAxis);
-
-    const line = d3
-      .line()
-      .x(d => d[0])
-      .y(d => d[1])
-      .curve(d3.curveMonotoneX);
-    const points: [number, number][] = this.data.map(d => [this.xScale(new Date(d.date)), this.yScale(d.value)]);
-    this.lineGroup.attr("d", line(points));
   }
 }
